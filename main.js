c671f2eff9373b9d78285f033e52c2f5fd42101e
Hamzehomat <146542807+Hamzehomat@users.noreply.github.com>
2024-02-10 14:17:46 +0300

    js_task7

---
+++ main.js
@@ -0,0 +1,28 @@
+const button1 = document.querySelector(".num1");
+let result=0;
+button1.onclick=function(){
+   result++;
+   document.querySelector(".value1").textContent=result;
+console.log(result);
+};
+const button2 = document.querySelector(".num2");
+let result2=0;
+button2.onclick=function(){
+   result2++;
+   document.querySelector(".value2").textContent=result2;
+console.log(result2);
+};
+const button3 = document.querySelector(".num3");
+let result3=0;
+button3.onclick=function(){
+   result3++;
+   document.querySelector(".value3").textContent=result3;
+console.log(result3);
+};
+const button4 = document.querySelector(".num4");
+let result4=0;
+button4.onclick=function(){
+   result4++;
+   document.querySelector(".value4").textContent=result4;
+console.log(result4);
+};
